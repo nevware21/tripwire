10a480e0c5185df673d76932a6d19343950aa37c
Nev <nev@nevware21.com>
2024-09-21 22:44:26 -0700

    Update to ts-utils 0.11.4

---
+++ core/test/src/support/validate.symbol.test.ts
@@ -0,0 +1,22 @@
+/*
+ * @nevware21/tripwire
+ * https://github.com/nevware21/tripwire
+ *
+ * Copyright (c) 2024 NevWare21 Solutions LLC
+ * Licensed under the MIT license.
+ */
+
+import { getKnownSymbol } from "@nevware21/ts-utils";
+import { WellKnownSymbols } from "@nevware21/ts-utils";
+import { assert } from "../../../src/assert/assertClass";
+
+// Define the test suite
+describe("symbol check", () => {
+
+    // Test case: null object
+    it("Should fail for null object", () => {
+        const knownIterator = getKnownSymbol(WellKnownSymbols.iterator)
+
+        assert.strictEqual(knownIterator, Symbol.iterator);
+    });
+});
